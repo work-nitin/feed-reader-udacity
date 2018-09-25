587eb762aa6dd3c0b5b6b2bbfae5f446e0d0ef1a
nitin batra <work.nitinbatra@gmail.com>
2018-09-25 15:07:54 -0700

    Initial Commit -Feedreader

---
+++ README.md
@@ -0,0 +1,62 @@
+# Project Overview
+Its a web-based application that reads RSS feeds.
+# Steps to test the HTML and Javascript with Jasmine Testing Tool
+Open the index.HTML and once Jasime is installed you should see the Jasmine test window at the bottom of your HTML once DOM is loaded fully.
+
+Documentation and started guides:
+ http://jasmine.github.io For a quick start guide of Jasmine, see the beginning of http://jasmine.github.io/edge/introduction.html
+
+
+ Installation
+ For the Jasmine NPM module:
+ https://github.com/jasmine/jasmine-npm
+
+ For the Jasmine Ruby Gem:
+ https://github.com/jasmine/jasmine-gem
+
+ For the Jasmine Python Egg:
+ https://github.com/jasmine/jasmine-py
+
+ For the Jasmine headless browser gulp plugin:
+ https://github.com/jasmine/gulp-jasmine-browser
+
+ To install Jasmine standalone on your local box (where {#.#.#} below is substituted by the release number downloaded):
+
+ Download the standalone distribution for your desired release from the releases page
+ Create a Jasmine directory in your project - mkdir my-project/jasmine
+ Move the dist to your project directory - mv jasmine/dist/jasmine-standalone-{#.#.#}.zip my-project/jasmine
+ Change directory - cd my-project/jasmine
+ Unzip the dist - unzip jasmine-standalone-{#.#.#}.zip
+ Add the following to your HTML file:
+
+ <link rel="shortcut icon" type="image/png" href="jasmine/lib/jasmine-{#.#.#}/jasmine_favicon.png">
+ <link rel="stylesheet" type="text/css" href="jasmine/lib/jasmine-{#.#.#}/jasmine.css">
+
+ <script type="text/javascript" src="jasmine/lib/jasmine-{#.#.#}/jasmine.js"></script>
+ <script type="text/javascript" src="jasmine/lib/jasmine-{#.#.#}/jasmine-html.js"></script>
+ <script type="text/javascript" src="jasmine/lib/jasmine-{#.#.#}/boot.js"></script>
+
+## Why this Project?
+Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
+
+Whether you work in an organization that uses test-driven development or in an organization that uses tests to make sure future feature development doesn't break existing features, it's an important skill to have!
+
+## What will I learn?
+
+You will learn how to use Jasmine to write a number of tests against a pre-existing application. These will test the underlying business logic of the application as well as the event handling and DOM manipulation.
+
+## How will this help my career?
+
+* Writing effective tests requires analyzing multiple aspects of an application including the HTML, CSS and JavaScript - an extremely important skill when changing teams or joining a new company.
+* Good tests give you the ability to quickly analyze whether new code breaks an existing feature within your codebase, without having to manually test all of the functionality.
+
+Maintainers
+Gregg Van Hove, Pivotal Labs
+Maintainers Emeritus
+Davis W. Frank, Pivotal Labs
+Rajan Agaskar, Pivotal Labs
+Greg Cobb, Pivotal Labs
+Chris Amavisca, Pivotal Labs
+Christian Williams, Cloud Foundry
+Sheel Choksi
+Copyright (c) 2008-2018 Pivotal Labs. This software is licensed under the MIT License.
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,102 @@
+/* This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against our application.*/
+
+$( function() {
+
+	let startFeed
+		, endFeed;
+	/********************************suite #1 ****************************************************************************************************************/
+	/* This is our first test suite - This suite is all about the RSS
+	 * feeds definitions, the allFeeds variable in our application.
+	 */
+	describe( 'Testing for <Udacity RSS Feeds> definitions -> ', function() {
+
+		/* This is our first test - it tests to make sure that the
+		 * allFeeds variable has been defined and that it is not
+		 * empty.
+		 */
+		it( ' Test case #1 => All RSS feeds are defined correctly', function() {
+			expect( allFeeds ).toBeDefined();
+			expect( allFeeds.length ).not.toBe( 0 );
+		} );
+
+		/* This is our Second test - it tests to make sure that URL is defined properly
+		 */
+		it( ' Test case #2 => my url property defined correctly', function() {
+			for ( let feed of allFeeds ) {
+				expect( feed.url ).toBeDefined();
+				expect( feed.url.length ).not.toBe( 0 );
+			}
+		} );
+		/* This is our Third test - it tests to make sure that Name proporty is defined properly
+		 */
+		it( ' Test case #3 => my name property defined correctly', function() {
+			for ( let feed of allFeeds ) {
+				expect( feed.name ).toBeDefined();
+				expect( feed.name.length ).not.toBe( 0 );
+			}
+		} );
+	} );
+
+	/************************************suite #2 ***************************************************************************************************/
+	describe( 'Testing for <The Menu> definitions -> ', function() {
+
+		/* This is our 4th test - it tests to ensures the menu element is
+		 * hidden by default.
+		 */
+		it( ' Test case #4 => Menu hidden by default', function() {
+			const body = document.querySelector( 'body' );
+			expect( body.classList.contains( 'menu-hidden' ) ).toBe( true );
+		} );
+
+		/* This is our 5th test - test that ensures the menu changes visibility when the menu icon is clicked. This test
+		 * will also test two more expectations: does the menu display when clicked and does it hide when clicked again.*/
+		it( ' Test case #5 => Menu toggles ', function() {
+			const body = document.querySelector( 'body' );
+			const menu = document.querySelector( '.menu-icon-link' );
+			menu.click();
+			expect( body.classList.contains( 'menu-hidden' ) ).toBe( false );
+			menu.click();
+			expect( body.classList.contains( 'menu-hidden' ) ).toBe( true );
+		} );
+
+	} );
+
+	/************************************suite #3 ***********************************************************************************************/
+	describe( 'Testing for <Initial Entries> definitions -> ', function() {
+		beforeEach( function( done ) {
+			loadFeed( 0, done );
+		} );
+		/* This is our 6th test*/
+		it( ' Test case #6 => loadFeed function works correctly', function() {
+			const entryElement = document.querySelector( '.feed' );
+			expect( entryElement.children.length > 0 ).toBe( true );
+		} );
+
+	} );
+	/************************************suite #4 ************************************************************************************************/
+	/* TODO: Write a test that ensures when a new feed is loaded
+	 * by the loadFeed function that the content actually changes.
+	 * Remember, loadFeed() is asynchronous.
+	 */
+	describe( 'Testing for <New Feed Selection> definitions -> ', function() {
+		const feed = document.querySelector( '.feed' );
+		beforeEach( function( done ) {
+			loadFeed( 0, function() {
+				startFeed = $( '.feed' ).find( allFeeds.url );
+				done();
+			} );
+			loadFeed( 1, function() {
+				endFeed = $( '.feed' ).find( allFeeds.url );
+			} );
+		} );
+		/* This is our 7th test*/
+		it( ' Test case #7 => loadFeed function content changes and is not equal', function() {
+			expect( startFeed ).not.toBe( endFeed );
+
+		} );
+
+	} );
+
+}() );
+/************************************************************************************************************************************************/
